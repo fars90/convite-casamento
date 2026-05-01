ff57abcf6f12c642a4b46991d33b709b913d302a
fars90 <andreraposeirofaria@gmail.com>
2026-05-01 23:30:26 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const envelope = document.getElementById("envelope");
 const instruction = document.getElementById("instruction");
+const music = document.getElementById("music");
+const muteButton = document.getElementById("muteButton");
+const volumeSlider = document.getElementById("volumeSlider");
 
 let step = 0;
 
+if (music) {
+  music.volume = 0.5;
+}
+
 envelope.addEventListener("click", () => {
+  // tocar música no primeiro clique
   if (step === 0) {
+    music.play().catch(() => {});
     envelope.classList.add("turned");
     step = 1;
   } else if (step === 1) {
@@ -15,5 +24,24 @@ envelope.addEventListener("click", () => {
       envelope.classList.add("final");
       instruction.classList.add("hide");
     }, 2800);
+  }
+});
+
+muteButton.addEventListener("click", (event) => {
+  event.stopPropagation();
+
+  music.muted = !music.muted;
+  muteButton.textContent = music.muted ? "🔇" : "🔊";
+});
+
+volumeSlider.addEventListener("input", (event) => {
+  music.volume = event.target.value;
+
+  if (music.volume == 0) {
+    music.muted = true;
+    muteButton.textContent = "🔇";
+  } else {
+    music.muted = false;
+    muteButton.textContent = "🔊";
   }
 });
